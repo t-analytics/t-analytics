642dd0c45b2a8e821b3d681a305dfeb9baa20825
ryakubovsky <data.yakubovsky.rom@gmail.com>
2020-05-04 15:23:23 +0300

    update vk webhook

--- webhooks_analytics/views.py
+++ webhooks_analytics/views.py
@@ -1,10 +1,30 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
+import requests
 # Create your views here.
 
 
 def webhook_vkontakte(request, client_id):
-    message = "Not get param"
+    https_proxy1 = "https://54.164.133.248:3128"
+    https_proxy2 = "https://163.172.219.130:443"
+    https_proxy3 = "https://151.253.165.70:8080"
+
+    access_token = "1074300704:AAGCu4LpIOk0Wq2Z9gzkUTiSjbt_wT0MxeQ"
+
+    proxyDict = {
+        "https": https_proxy1,
+        "https": https_proxy2,
+        "https": https_proxy3
+    }
     if request.method == 'GET':
-        message = request.GET.get("hello")
-    return HttpResponse(f"{message} - {client_id}")
+        if request.GET.get("type") == 'confirmation':
+            return HttpResponse('10a829f5')
+        else:
+            return HttpResponse('')
+    elif request.method == 'POST':
+        if request.POST.get("type") == 'confirmation':
+            return HttpResponse('10a829f5')
+        else:
+            return HttpResponse('')
+    else:
+        return HttpResponse('')
